47913c56e3f3393aed7f68bf0003aff88929df0c
MDhabees <164841099+MDhabees@users.noreply.github.com>
2024-03-25 11:10:33 +0530

    Update Exp1_Halfsubractor.v

--- Exp1_Halfsubractor.v
+++ Exp1_Halfsubractor.v
@@ -1,7 +1,13 @@
-module hs(a,b,difference,borrow);
-input a,b;
-output difference,borrow;
-
-
-
+// fullsubtractor using gate level modeling
+module full_sub(borrow,diff,a,b,c);
+output borrow,diff;
+input a,b,c;
+wire w1,w4,w5,w6;
+xor (diff,a,b,c);
+not n1(w1,a);
+and a1(w4,w1,b);
+and a2(w5,w1,c);
+and a3(w6,b,c);
+or o1(borrow,w4,w5,w6);
 endmodule
+
